f175bf4411f2404c602893c85973e900f066b755
unknown <billelmh501@gmail.com>
2025-12-30 17:27:09 +0100

    add the logout command handler

---
+++ backend/Veterinary/src/VeterinaryApi/Features/Users/Logout.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using VeterinaryApi.Common.Abstracions;
+using VeterinaryApi.Common.CQRS;
+using VeterinaryApi.Common.Results;
+using VeterinaryApi.Domain.Users;
+
+namespace VeterinaryApi.Features.Users;
+
+public class Logout
+{
+    public sealed record LogoutCommand(string RefreshToken)
+        : ICommand;
+
+    public sealed class LogoutCommandHandler
+        : ICommandHandler<LogoutCommand>
+    {
+        private readonly IApplicationDbContext _db;
+
+        public LogoutCommandHandler(IApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Result> Handle(LogoutCommand command, CancellationToken cancellationToken = default)
+        {
+            var session = await _db.UserSessions
+                .FirstOrDefaultAsync(e => e.Token == command.RefreshToken);
+            if (session is null)
+            {
+                return Result.Failure(UserErrors.InvalidCredentials);
+            }
+            _db.UserSessions.Remove(session);
+            await _db.SaveChangesAsync(cancellationToken);
+            return Result.Success;
+        }
+    }
+}
